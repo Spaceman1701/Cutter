5ec89b678b2b7268db7d5f10ac84ea0fd415f25f
Ethan Hunter <fc.spaceman@gmail.com>
2017-08-25 23:46:35 -0600

    add VoidPointCut

---
+++ cutter-lib/src/main/java/org/x2a/cutter/pointcut/VoidPointCut.java
@@ -0,0 +1,25 @@
+package org.x2a.cutter.pointcut;
+
+import org.x2a.cutter.cut.JoinPoint;
+import org.x2a.cutter.cut.Parameter;
+import org.x2a.cutter.cut.PointCut;
+
+public abstract class VoidPointCut extends PointCut<Void> {
+    public VoidPointCut(JoinPoint joinPoint, Parameter[] parameters) {
+        super(joinPoint, parameters);
+    }
+
+    public abstract void after();
+    @Override
+    public Void after(Void v) {
+        after();
+        return null;
+    }
+
+    public abstract void skip();
+    @Override
+    public Void onSkip() {
+        skip();
+        return null;
+    }
+}
