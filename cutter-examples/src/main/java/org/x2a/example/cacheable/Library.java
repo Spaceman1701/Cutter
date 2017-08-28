25507494a8635b501be58dd5657c3cdc1d3dc97e
Ethan Hunter <fc.spaceman@gmail.com>
2017-08-28 09:06:03 -0600

    stylisitc changes to the example project

--- cutter-examples/src/main/java/org/x2a/example/cacheable/Library.java
+++ cutter-examples/src/main/java/org/x2a/example/cacheable/Library.java
@@ -7,8 +7,7 @@ import org.x2a.example.cacheable.cahce.Cacheable;
 public class Library {
 
 
-    @Cut(Cacheable.class)
-    @CacheParams(key = "title")
+    @Cut(Cacheable.class) @CacheParams(key = "title")
     public Book findBook(String title) {
         System.out.println("Getting " + title + " from the library");
         //expensive search here
