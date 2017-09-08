1dcd9dde9682493934d97a75f8d2a592390e312b
Ethan Hunter <fc.spaceman@gmail.com>
2017-09-07 21:55:16 -0600

    add javadoc to RequiredAnnotations

--- cutter-compiler/src/main/java/org/x2a/cutter/annotation/RequiredAnnotations.java
+++ cutter-compiler/src/main/java/org/x2a/cutter/annotation/RequiredAnnotations.java
@@ -2,6 +2,11 @@ package org.x2a.cutter.annotation;
 
 import java.lang.annotation.*;
 
+/**
+ * Applies to {@link org.x2a.cutter.cut.Advice} class definitions.
+ * <p>
+ * Define annotations required to be present whenever the targeted Advice is used. Requirements are verified at compile-time
+ */
 @Documented @Inherited
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.CLASS)
